77d687f627bf79bd27f7c873b88bd216a1cef655
Eugene Lezov <ELezov@stud.kantiana.ru>
2017-01-12 14:37:14 +0200

    Update
    Update Clicker Project && Co

--- KotlinAPP/app/src/main/java/eugene/com/kotlinapp/MainActivity.kt
+++ KotlinAPP/app/src/main/java/eugene/com/kotlinapp/MainActivity.kt
@@ -1,10 +1,11 @@
 package eugene.com.kotlinapp
 
+import android.content.Context
 import android.content.Intent
+import android.content.SharedPreferences
+import android.media.MediaPlayer
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.os.PersistableBundle
-import android.widget.Button
 import android.widget.ImageButton
 import android.widget.TextView
 import java.util.*
@@ -14,24 +15,40 @@ import kotlin.concurrent.timerTask
 
 class MainActivity : AppCompatActivity() {
 
-    private val POINT_KEY= "point_key"
-    private val TIME_KEY="time_key"
+    companion object{
+        private val POINT_KEY= "point_key"
+        private val TIME_KEY="time_key"
+        private val SHARED_KEY="clicker_shared"
+        private  val SHARED_KEY_POINT="clicker_shared_point"
+        private var mpBackground =MediaPlayer()
+        private var mp=MediaPlayer()
+    }
+
+
 
     private var time=25
     private var point=0
 
+    private var flag=false
+
     private var PointText:TextView? =null
     private var TimeText:TextView? =null
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        //if (savedInstanceState==null)
+         //   PointText!!.text=getIntPrefereces(SHARED_KEY_POINT).toString()
+
+        mpBackground=MediaPlayer.create(applicationContext,R.raw.background_trak)
+        mpBackground.start()
+        mpBackground.setOnCompletionListener{ mpBackground.start()}
+
         var ClickBtn = findViewById(R.id.mybutton) as ImageButton
         PointText=findViewById(R.id.point_text) as TextView
         TimeText=findViewById(R.id.time_text) as TextView
-
-
 
         ClickBtn.setOnClickListener {
             point++;
@@ -45,12 +62,29 @@ class MainActivity : AppCompatActivity() {
                 TimeText!!.post{
                     TimeText!!.text=(time/60).toString() + " : "+ (time%60).toString()
                 }
+                var random=(Math.random()*10).toInt()
+                if (!mp.isPlaying) {
+                    when(random){
+                        1 -> { mp = MediaPlayer.create(applicationContext, R.raw.trak_2);mp.start()}
+                        3-> {mp = MediaPlayer.create(applicationContext, R.raw.trak_1);mp.start()}
+                        7-> {mp = MediaPlayer.create(applicationContext, R.raw.trak_3);mp.start()}
+                    }
+                }
+
                 if (time<=0) {
                     ClickBtn.post{
                         ClickBtn.isEnabled=false;
                     }
 
+                    if (getIntPrefereces(SHARED_KEY_POINT)< point) {
+                        setIntPreferences(SHARED_KEY_POINT, point)
+                        flag=true;
+                    }
                     var intent = Intent(applicationContext,ResultActivity::class.java)
+                    intent.putExtra(ResultActivity.INTENT_KEY,point)
+                    intent.putExtra(ResultActivity.INTENT_POINT,getIntPrefereces(SHARED_KEY_POINT))
+                    intent.putExtra(ResultActivity.INTENT_RESULT,flag)
+
                     startActivity(intent)
                     timer.cancel()
 
@@ -74,7 +108,31 @@ class MainActivity : AppCompatActivity() {
             point = savedInstanceState.getInt(POINT_KEY)
             time=savedInstanceState.getInt(TIME_KEY)
             PointText!!.text=point.toString()
-            TimeText!!.text=time.toString()
+            TimeText!!.text=(time/60).toString()+":"+(time % 60).toString()
         }
+    }
+
+    private fun setIntPreferences(key:String,point:Int) {
+        val sh = getSharedPreferences(SHARED_KEY, Context.MODE_PRIVATE)
+        val editor = sh.edit()
+        editor.putInt(SHARED_KEY_POINT, point)
+        editor.apply()
+    }
+
+    private  fun getIntPrefereces(key:String):Int{
+        val sh=getSharedPreferences(SHARED_KEY,Context.MODE_PRIVATE)
+        return sh.getInt(key,0)
+    }
+
+    override fun onStop() {
+        super.onStop()
+        mp.stop()
+        mpBackground.stop()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        //mp.stop()
+        //mpBackground.stop()
     }
 }
--- KotlinAPP/app/src/main/java/eugene/com/kotlinapp/ResultActivity.kt
+++ KotlinAPP/app/src/main/java/eugene/com/kotlinapp/ResultActivity.kt
@@ -3,18 +3,53 @@ package eugene.com.kotlinapp
 import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.util.Log
 import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
+import org.w3c.dom.Text
 
 /**
  * Created by USER on 15.12.2016.
  */
 class ResultActivity : AppCompatActivity() {
 
-    public val INTENT_KEY = "intent_key"
+
+    companion object {
+        val INTENT_KEY = "intent_key"
+        val INTENT_POINT="intent_point"
+        val INTENT_RESULT="intent_result"
+    }
+
+    private var recText:TextView? =null
+    private var TextPoint:TextView? =null
+    //private var result_img:ImageView? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_result)
+
+        recText = findViewById(R.id.record_text) as TextView
+        TextPoint=findViewById(R.id.result_text) as TextView
+        var result_img=findViewById(R.id.result_img) as ImageView
+
+        var point=intent.getIntExtra(INTENT_KEY,0)
+        var flag=intent.getBooleanExtra(INTENT_RESULT,false)
+        var point_record=intent.getIntExtra(INTENT_POINT,0)
+
+        Log.d("my_log",point.toString())
+
+        when{
+            point<0.7*point_record-> result_img.setImageDrawable(applicationContext.resources.getDrawable(R.drawable.frightened))
+            point>=0.7*point_record&&point<point_record->result_img.setImageDrawable(applicationContext.resources.getDrawable(R.drawable.sad))
+            point>=point_record->result_img.setImageDrawable(applicationContext.resources.getDrawable(R.drawable.smile))
+        }
+
+
+
+        TextPoint!!.text=point.toString()
+        if (flag==true) recText!!.text="Вы сделали новый рекорд"
+        else recText!!.text="Текущий рекорд = "+point_record.toString()
 
         var RepeatBtn=findViewById(R.id.restart_btn) as Button
 
